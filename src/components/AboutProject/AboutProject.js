feef7eaba0e1e3ef8db8df08efabad1c089a9378
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-07-06 05:51:52 +0300

    main page

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,9 +1,9 @@
 import './AboutProject.css';
-
+import Title from '../Title/Title';
 function AboutProject() {
     return (
         <section className="about-project" id='about-project'>
-            <h2 className='about-project__title'>О Проекте</h2>
+            <Title text={"О Проекте"} />
             <div className='about-project__container'>
                 <div className='about-project__info'>
                     <h3 className='about-project__info-title'>Дипломный проект влючал 5 этапов</h3>
@@ -12,6 +12,17 @@ function AboutProject() {
                 <div className='about-project__info'>
                     <h3 className='about-project__info-title'>На выполнение диплома ушло 5 недель</h3>
                     <p className='about-project__info-text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+            </div>
+            
+            <div className='about-project__container'>
+                <div className='timeline'>
+                    <h4 className='timeline__title'>1 неделя</h4>
+                    <p className='timeline__text'>Back-end</p>
+                </div>
+                <div className='timeline'>
+                    <h4 className='timeline__title timeline__title_type_light'>4 недели</h4>
+                    <p className='timeline__text'>Front-end</p>
                 </div>
             </div>
         </section>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,24 @@
+import './Portfolio.css'
+
 function Portfolio() {
     return (
-        <div className="">
-            
+        <div className="portfolio">
+            <h2 className="portfolio__title">Портфолио</h2>
+            <ul className="portfolio__list">
+                <li className="portfolio__list-item">
+                    <a className='portfolio__link' href="https://github.com/RuslanVoronov/first-project/tree/main" target='_blank' rel="noreferrer">Статичный сайт</a>
+                    <a className='portfolio__arrow' href="https://github.com/RuslanVoronov/first-project/tree/main" target='_blank' rel="noreferrer">↗</a>
+                </li>
+                <li className="portfolio__list-item">
+                    <a className='portfolio__link' href="https://github.com/RuslanVoronov/russian-travel" target='_blank' rel="noreferrer">Адаптивный сайт</a>
+                    <a className='portfolio__arrow' href="https://github.com/RuslanVoronov/first-project/tree/main" target='_blank' rel="noreferrer">↗</a>
+                </li>
+                <li className="portfolio__list-item">
+                    <a className='portfolio__link' href="https://github.com/RuslanVoronov/react-mesto-api-full-gha" target='_blank' rel="noreferrer">Одностраничное приложение</a>
+                    <a className='portfolio__arrow' href="https://github.com/RuslanVoronov/first-project/tree/main" target='_blank' rel="noreferrer">↗</a>
+                </li>
+            </ul>
+
         </div>
     );
 }
